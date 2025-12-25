23623cb1bd4e084e293174392ba708f1695a1283
Thomas Adamcik <thomas@adamcik.no>
2025-12-25 22:17:44 +0100

    Switch to treefmt

--- flake.nix
+++ flake.nix
@@ -3,11 +3,14 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+    treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = {
     self,
     nixpkgs,
+    treefmt-nix,
     ...
   }: let
     forAllSystems =
@@ -19,25 +22,24 @@
         "aarch64-darwin"
         "x86_64-darwin"
       ];
+    treefmtEval = forAllSystems (system:
+      treefmt-nix.lib.evalModule nixpkgs.legacyPackages.${system} {
+        projectRootFile = "flake.nix";
+        programs.alejandra.enable = true;
+        programs.prettier.enable = true;
+        programs.gofmt.enable = true;
+      });
   in {
-    formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.alejandra);
+    formatter = forAllSystems (system: treefmtEval.${system}.config.build.wrapper);
     packages = forAllSystems (
       system:
         import ./pkgs {
           pkgs = nixpkgs.legacyPackages.${system};
         }
     );
-    checks = forAllSystems (system: let
-      pkgs = nixpkgs.legacyPackages.${system};
-    in
+    checks = forAllSystems (system:
       {
-        format =
-          pkgs.runCommand "check-format" {
-            nativeBuildInputs = [pkgs.alejandra];
-          } ''
-            alejandra --check ${self}
-            touch $out
-          '';
+        formatting = treefmtEval.${system}.config.build.check self;
       }
       // self.packages.${system});
     nixosModules.fleet-nixos = import ./modules {
